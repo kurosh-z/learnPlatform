abd70e01e28b0a39068fc2d30cc41f5103ed7372
Kurosh Zamani <kurosh.zamany@gmail.com>
2020-05-21 13:52:10 +0200

    without router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React, { Suspense, lazy } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HomePage from './homepage/HomePage'
+// import HomePage from './homepage/HomePage'
 
-const ExampleCourse = lazy(() =>
-    import(/* webpackPrefetch: true */ './Courses/exampleCourse/ExampleCourse')
-)
+// const ExampleCourse = lazy(() =>
+//     import(/* webpackPrefetch: true */ './Courses/exampleCourse/ExampleCourse')
+// )
 
 import './App.css'
 
 const App: React.FC<{}> = () => {
-    return (
-        <Suspense fallback={null}>
-            <Router>
-                <Switch>
-                    <Route path="/" exact component={HomePage} />
-                    <Route path="/courses" component={ExampleCourse} />
-                </Switch>
-            </Router>
-        </Suspense>
-    )
+    return <HomePage />
 }
 
 export default App
